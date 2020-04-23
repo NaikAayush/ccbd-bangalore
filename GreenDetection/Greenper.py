c40dc18c069575af5452bef62fd8a026a95461de
varunp2k <varunp2k@gmail.com>
2020-04-24 00:05:25 +0530

    Code for greenery Detection using KMEANS ans normal OPENCV

--- GreenDetection/Greenper.py
+++ GreenDetection/Greenper.py
@@ -1,17 +1,40 @@
+# import cv2
+# import numpy as np
+
+# img = cv2.imread('/home/etherealenvy/github/ccbd-bangalore/GreenDetection/Images_Vertical_1024px/23428/15175.jpg')
+# size = img.size
+
+# #replace green threshold in BGR form cause openCV reads numpy arrays in reverse order
+# GREEN_MIN = np.array([0,0,0], np.uint8)
+# GREEN_MAX = np.array([250, 255, 255], np.uint8)
+
+
+# dstr = cv2.inRange(img, GREEN_MIN, GREEN_MAX)
+# no_GREEN = cv2.countNonZero(dstr)
+
+# frac_GREEN = np.divide((float(no_GREEN)),(float(size)))
+# percent_GREEN = np.multiply((float(frac_GREEN)), 100)
+# print('GREEN: ' + str(percent_GREEN) + '%')
+
 import cv2
 import numpy as np
 
-img = cv2.imread('roi.jpg')
-size = img.size
+## Read
+abs_path = "/home/etherealenvy/github/ccbd-bangalore/GreenDetection/Images_Vertical_1024px/23428/15175.jpg"
+img = cv2.imread(abs_path)
+name = abs_path.split("/")[-2]+"/"+abs_path.split("/")[-1]
+# print(name)
+## convert to hsv
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-#replace green threshold
-GREEN_MIN = np.array([0,0,128], np.uint8)
-GREEN_MAX = np.array([250, 250, 255], np.uint8)
+## mask of green (36,25,25) ~ (86, 255,255)
+# mask = cv2.inRange(hsv, (36, 25, 25), (86, 255,255))
+mask = cv2.inRange(hsv, (35, 25, 25), (160, 255,255))
 
+## slice the green
+imask = mask>0
+green = np.zeros_like(img, np.uint8)
+green[imask] = img[imask]
 
-dstr = cv2.inRange(img, GREEN_MIN, GREEN_MAX)
-no_GREEN = cv2.countNonZero(dstr)
-
-frac_GREEN = np.divide((float(no_GREEN)),(float(size)))
-percent_red = np.multiply((float(frac_GREEN)), 100)
-print('GREEN: ' + str(percent_GREEN) + '%')
+## save 
+cv2.imwrite(name, green)
--- GreenDetection/allcolorDetection.py
+++ GreenDetection/allcolorDetection.py
@@ -5,8 +5,11 @@ import cv2
 from collections import Counter
 from skimage.color import rgb2lab, deltaE_cie76
 import os
-
 %matplotlib inline
+
+abs_path = "/home/etherealenvy/github/ccbd-bangalore/GreenDetection/Images_Vertical_1024px/23428/15175.jpg"
+img = cv2.imread(abs_path)
+name = abs_path.split("/")[-2]+"/"+abs_path.split("/")[-1]
 
 
 def RGB2HEX(color):
@@ -14,7 +17,7 @@ def RGB2HEX(color):
 
 def get_image(image_path):
     image = cv2.imread(image_path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
 def get_colors(image, number_of_colors, show_chart):
@@ -39,39 +42,58 @@ def get_colors(image, number_of_colors, show_chart):
 
     return rgb_colors
     
-get_colors(get_image(‘sample_image.jpg’), 8, True)
+get_colors(get_image(abs_path), 8, True)
 
 
 
-#We’ll now dive into the code of filtering a set of five images based on the color we’d like
-IMAGE_DIRECTORY = 'images'
-COLORS = {
-    'GREEN': [0, 128, 0],
-    'BLUE': [0, 0, 128],
-    'YELLOW': [255, 255, 0]
-}
-images = []
+# #We’ll now dive into the code of filtering a set of five images based on the color we’d like
+# IMAGE_DIRECTORY = 'images'
+# COLORS = {
+#     'GREEN': [0, 128, 0],
+#     'BLUE': [0, 0, 128],
+#     'YELLOW': [255, 255, 0]
+# }
+# images = []
 
-for file in os.listdir(IMAGE_DIRECTORY):
-    if not file.startswith('.'):
-        images.append(get_image(os.path.join(IMAGE_DIRECTORY, file)))
+# for file in os.listdir(IMAGE_DIRECTORY):
+#     if not file.startswith('.'):
+#         images.append(get_image(os.path.join(IMAGE_DIRECTORY, file)))
 
-#show all images we will be using
-# plt.figure(figsize=(20, 10))
-# for i in range(len(images)):
-#     plt.subplot(1, len(images), i+1)
-#     plt.imshow(images[i])
+# #show all images we will be using
+# # plt.figure(figsize=(20, 10))
+# # for i in range(len(images)):
+# #     plt.subplot(1, len(images), i+1)
+# #     plt.imshow(images[i])
 
-# to filter all images that match the selected color.
-def match_image_by_color(image, color, threshold = 60, number_of_colors = 10): 
+# # to filter all images that match the selected color.
+# def match_image_by_color(image, color, threshold = 60, number_of_colors = 10): 
     
-    image_colors = get_colors(image, number_of_colors, False)
-    selected_color = rgb2lab(np.uint8(np.asarray([[color]])))
+#     image_colors = get_colors(image, number_of_colors, False)
+#     selected_color = rgb2lab(np.uint8(np.asarray([[color]])))
 
-    select_image = False
-    for i in range(number_of_colors):
-        curr_color = rgb2lab(np.uint8(np.asarray([[image_colors[i]]])))
-        diff = deltaE_cie76(selected_color, curr_color)
-        if (diff < threshold):
-            select_image = True
+#     select_image = False
+#     for i in range(number_of_colors):
+#         curr_color = rgb2lab(np.uint8(np.asarray([[image_colors[i]]])))
+#         diff = deltaE_cie76(selected_color, curr_color)
+#         if (diff < threshold):
+#             select_image = True
     
+#     return select_image
+
+# def show_selected_images(images, color, threshold, colors_to_match):
+#     index = 1
+    
+#     for i in range(len(images)):
+#         selected = match_image_by_color(images[i],
+#                                         color,
+#                                         threshold,
+#                                         colors_to_match)
+#         if (selected):
+#             plt.subplot(1, 5, index)
+#             plt.imshow(images[i])
+#             index += 1
+#     return True
+
+# # Variable 'selected_color' can be any of COLORS['GREEN'], COLORS['BLUE'] or COLORS['YELLOW']
+# plt.figure(figsize = (20, 10))
+# show_selected_images(images, selected_color, 60, 5)
