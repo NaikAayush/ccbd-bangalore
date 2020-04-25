f605b31409cde465247114e59445ba53f4d28247
Samyak S Sarnayak <samyak201@gmail.com>
2020-04-25 22:05:13 +0530

    Sort mapper output before reducing

--- MapReduce/app.py
+++ MapReduce/app.py
@@ -1,3 +1,4 @@
+import subprocess
 import csv
 from flask import Flask, render_template, request
 import mapper
@@ -12,19 +13,20 @@ def index():
         green_percentage = float(green_percentage)
         print("Using green_percentage:", green_percentage)
         with open("main.csv", "rt") as input_file:
-            with open("mapper_out", "wt+") as mapper_out:
-                with open("output.csv", "wt") as output_file:
-                    print("Running mapper")
+            with open("output.csv", "wt") as output_file:
+                print("Running mapper")
+                with open("mapper_out", "wt") as mapper_out:
                     mapper.run_map_task(input_file, mapper_out)
-                    mapper_out.seek(0)
-                    print("Runnnng reducer")
+                subprocess.call(["sort", "-k1,1", "mapper_out", "-o", "mapper_out"])
+                print("Runnnng reducer")
+                with open("mapper_out", "rt") as mapper_out:
                     reducer.run_reduce_task(green_percentage, mapper_out, output_file)
         outputs = []
         with open("output.csv", "rt") as output_file:
             reader = csv.reader(output_file, delimiter=" ")
             for row in reader:
                 outputs.append(row)
-        return render_template("index.html", outputs=outputs, green_percentage=green_percentage)
+        return render_template("index.html", outputs=outputs, green_percentage=str(green_percentage))
     return render_template("index.html")
 
 if __name__ == "__main__":
