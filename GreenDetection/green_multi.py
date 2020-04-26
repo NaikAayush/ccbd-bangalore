1183018d5a67c406f61f1a72bbf4f5385391422c
Samyak S Sarnayak <samyak201@gmail.com>
2020-04-26 19:59:27 +0530

    Fixed small issue with multithreaded green script

--- GreenDetection/green_multi.py
+++ GreenDetection/green_multi.py
@@ -13,20 +13,21 @@ output_folder_path = "../../Images_Vertical_1024px_green_23428"
 os.makedirs(output_folder_path, exist_ok=True)
 size = 1024*1024
 output_csv_prefix = "output_{}.csv"
+concatenated_csv = "output.csv"
 number_of_threads = 8
 
-def green(image_filenames, output_csv):
-    with open(output_csv, "at+") as csvfile:
+def greener(image_filenames, output_csv):
+    with open(output_csv, "wt+") as csvfile:
         # image_filenames = os.listdir(input_folder_path)
         # print("original images list:", image_filenames)
-        csvfile.seek(0)
-        csvreader = csv.reader(csvfile, delimiter=",")
-        skipped_count = 0
-        for row in csvreader:
-            img_file, green = row
-            image_filenames.remove(img_file)
-            skipped_count += 1
-        print("Skipped {} images already in CSV".format(skipped_count))
+        # csvfile.seek(0)
+        # csvreader = csv.reader(csvfile, delimiter=",")
+        # skipped_count = 0
+        # for row in csvreader:
+            # img_file, green = row
+            # image_filenames.remove(img_file)
+            # skipped_count += 1
+        # print("Skipped {} images already in CSV".format(skipped_count))
         # print("final images list:", image_filenames)
         csvwriter = csv.writer(csvfile, delimiter=",")
         for img_file in image_filenames:
@@ -69,11 +70,29 @@ if __name__ == "__main__":
     jobs = []
     output_csv_filenames = [output_csv_prefix.format(i) for i in range(number_of_threads)]
     image_filenames = os.listdir(input_folder_path)
+
+    # skip already processed images
+    csvfile = open(concatenated_csv, "at+")
+    csvfile.seek(0)
+    csvreader = csv.reader(csvfile, delimiter=",")
+    skipped_count = 0
+    for row in csvreader:
+        img_file, green = row
+        image_filenames.remove(img_file)
+        skipped_count += 1
+    print("Skipped {} images already in CSV".format(skipped_count))
+
     image_filenames_chunks = list(chunks(image_filenames, number_of_threads))
     for i, filenames_chunk, output_csv in zip(range(number_of_threads), image_filenames_chunks, output_csv_filenames):
-        p = multiprocessing.Process(target=green, args=(filenames_chunk, output_csv))
+        p = multiprocessing.Process(target=greener, args=(filenames_chunk, output_csv))
         p.start()
         jobs.append(p)
     for job in jobs:
         job.join()
-    print("Done")
+    print("Done processing. Now concatenating all CSVs into one.")
+
+    for output_csv in output_csv_filenames:
+        with open(output_csv, "rt") as output_csv_f:
+            for line in output_csv_f:
+                csvfile.write(line)
+    print("Done concatenating")
