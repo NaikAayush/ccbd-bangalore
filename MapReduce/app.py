ec960511dada79200b711f2ff3d5721e5522f38f
Samyak S Sarnayak <samyak201@gmail.com>
2020-04-27 08:27:47 +0530

    Updated web app for new data

--- MapReduce/app.py
+++ MapReduce/app.py
@@ -25,7 +25,7 @@ def mapRed():
         multithreaded = "multithreaded" in request.form
         hadoop = "hadoop" in request.form
 
-        inp_filename = "main_new.csv"
+        inp_filename = "FINAL_DATA_FILTERED.csv"
         if not hadoop:
             output_filename = "output.csv"
             if not multithreaded:
@@ -60,7 +60,8 @@ def mapRed():
                                outputs=outputs,
                                green_percentage=str(green_percentage),
                                time_taken=time_taken,
-                               multithreaded=multithreaded)
+                               multithreaded=multithreaded,
+                               hadoop=hadoop)
     return render_template("mapRed.html")
 
 if __name__ == "__main__":
